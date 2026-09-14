92be4e721aed20be69be854a5ba49066abc8bdfc
DAFZIE <daffa.akmal.p2000@gmail.com>
2026-09-14 22:23:49 +0700

    feat: Added a new page Project and added unit tests

--- main/tests.py
+++ main/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.utils import timezone
 
 from main.models import Experience
+from main.models import Project
 
 
 class MainTest(TestCase):
@@ -11,6 +12,12 @@ class MainTest(TestCase):
             title="Asisten Dosen PBP",
             description="Membantu mahasiswa memahami pengembangan web.",
             category="part-time",
+        )
+
+        self.project = Project.objects.create(
+            title="Concept Artist",
+            description="Make Concept Arts",
+            category="project-small",
         )
 
     def test_main_url_is_accessible(self):
@@ -56,3 +63,23 @@ class MainTest(TestCase):
         self.assertFalse(self.experience.is_ongoing)
         self.assertContains(response, "Selesai")
         self.assertNotContains(response, "Sedang berlangsung")
+
+    def test_project_model(self):
+        self.assertEqual(str(self.project), "Concept Artist")
+        self.assertEqual(self.project.category, "project-small")
+
+    def test_project_page(self):
+        response = self.client.get(reverse("main:show_project"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "project.html")
+        self.assertContains(response, self.project.title)
+        self.assertContains(response, self.project.description)
+        self.assertContains(response, "Small Project")
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+
+    def test_empty_project_page(self):
+        Project.objects.all().delete()
+        response = self.client.get(reverse("main:show_project"))
+
+        self.assertContains(response, "Belum ada proyek yang ditambahkan.")
